1cd00c2ef55a35bccab52addbb0d0a900eccf489
vinikuia <viniciuspinheiro.2016@alunos.utfpr.edu.br>
2021-05-11 19:42:52 -0300

    commit final trabalho 6

--- dados_de_rosto.py
+++ dados_de_rosto.py
@@ -6,38 +6,40 @@ def captura_dados_face():
 
     cap= cv2.VideoCapture(0, cv2.CAP_DSHOW)
     cascata_de_rosto = cv2.CascadeClassifier(cv2.haarcascades+ "haarcascade_frontalface_alt.xml")
+    # cascata_de_olho = cv2.CascadeClassifier(cv2.haarcascades+ "haarcascade_eye.xml")
+    # cascata_de_sorriso =  cv2.CascadeClassifier(cv2.haarcascades+ "haarcascade_smile.xml")
     skip = 0
     dados_de_rosto = []
     dataset_path = "./dados_de_rosto/"
 
     nome_do_arquivo = input("Qual o nome desse rosto??")
-
+    ret, frame = cap.read()
+    if ret != False:
+        frameShape = frame.shape
+        mask = np.zeros((frameShape[0], frameShape[1], 3), dtype=np.uint8)
     while True:
         ret,frame = cap.read()
 
         frame_escala_cinza = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         if ret == False:
             continue
-        rostos = cascata_de_rosto.detectMultiScale(frame_escala_cinza,1.3,5)
+        rostos = cascata_de_rosto.detectMultiScale(frame_escala_cinza,1.5,5)
         k=1
         rostos = sorted(rostos,key =lambda x : x[2]*x[3], reverse=True)
         skip +=1
-        for rosto in rostos[:1]:
+        for rosto in rostos:
             x,y,w,h = rosto
-
             offset = 5
-            rosto_offset = frame[y-offset:y+h+offset,x-offset:x+w+offset]
+            rosto_offset = frame_escala_cinza[y-offset:y+h+offset,x-offset:x+w+offset]
             try:
                 selecao_de_rosto = cv2.resize(rosto_offset,(100,100))
             except Exception as e:
                 # ignora imagens nas quais algum rosto esteja próximo a bora
                 print(str(e))
-            if skip % 10 == 0:
-                dados_de_rosto.append(selecao_de_rosto)
+            if skip % 5 == 0:
+                dados_de_rosto.append(rosto_offset)
                 print(len(dados_de_rosto))
 
-            frameShape = frame.shape
-            mask = np.zeros((frameShape[0], frameShape[1], 3), dtype=np.uint8)
             for i in range (y, y + h):
                 for j in range (x, x+w):
                     mask[i][j][:] = 255
@@ -48,9 +50,19 @@ def captura_dados_face():
             cv2.imshow(str(k),selecao_de_rosto)
             k+=1
 
-            cv2.imshow("rostos_borrados", blurredFace)
+            cv2.imshow("rostos_borrados", blurredFace )
 
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+            # olhos = cascata_de_olho.detectMultiScale(frame_escala_cinza,1.5,5)
+            # for olho in olhos:
+            #     ox, oy, ow, oh = olho
+            #     cv2.rectangle(frame, (ox, oy), (ox + ow, oy + oh), (0, 255, 0), 2)
+
+            # sorrisos = cascata_de_sorriso.detectMultiScale(frame_escala_cinza,2,5)
+            # for sorriso in sorrisos:
+            #     sx, sy, sw, sh = sorriso
+            #     cv2.rectangle(frame, (sx, sy), (sx + sw, sy + sh), (0, 255, 0), 2)
+
         cv2.imshow("rostos",frame)
         key_pressed = cv2.waitKey(1) & 0xFF
         if key_pressed == ord('q'):
--- face_recognition.py
+++ face_recognition.py
@@ -1,6 +1,14 @@
 import cv2
 import numpy as np
 import os
+import pylab as p
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC
+from sklearn.decomposition import PCA as RandomizedPCA
+from sklearn.model_selection import GridSearchCV
+from pathlib import Path
+import pickle
 
 def distance(v1,v2):
     # distancia euclidiana
@@ -24,73 +32,89 @@ def knn(train,test,k=5):
     index = np.argmax(output[1])
     return output[0][index]
 
+def face_recognition_trainer():
+    dataset_path = "./dados_de_rosto/"
+    if not os.path.exists(dataset_path):
+        raise Exception('Não há rostos cadastrados no sistema até então')
+    caminhos = [os.path.join(dataset_path, nome) for nome in os.listdir(dataset_path)]
+    arquivos = [arq for arq in caminhos if os.path.isfile(arq)]
+
+    faces = []
+
+    for arq in arquivos:
+        faces.append(np.load(arq))
+
+    categories = [arquivos.index(f) for f in arquivos]
+    names = [Path(f).stem for f in arquivos]
+    labels = [None] * len(categories)
+    for arq in arquivos:
+        labels[arquivos.index(arq)] = Path(arq).stem
+    with open("labels.pickle","wb") as f:
+        pickle.dump(labels,f)
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.train(faces,np.array(categories))
+    recognizer.save("trainner.yml")
+
+
 def face_recognition():
-    cap = cv2.VideoCapture(0)
-    cascata_de_rosto = cv2.CascadeClassifier(cv2.haarcascades+ "haarcascade_frontalface_alt.xml")
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read("trainner.yml")
+    labels = {}
+    with open("labels.pickle","rb") as f:
+        labels = pickle.load(f)
+    cascata_de_rosto = cv2.CascadeClassifier(cv2.haarcascades + "haarcascade_frontalface_alt.xml")
+    skip = 0
     dados_de_rosto = []
-    dataset_path = "./dados_de_rosto"
-    rotulos =[]
-    class_id = 0
-    nomes = {}
-    known_face_encodings = [
-        obama_face_encoding,
-        biden_face_encoding
-    ]
-    known_face_names = [
-        "Barack Obama",
-        "Joe Biden"
-    ]
+    dataset_path = "./dados_de_rosto/"
+
+
     while True:
-        # Grab a single frame of video
         ret, frame = cap.read()
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
+        frame_escala_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if ret == False:
+            continue
+        rostos = cascata_de_rosto.detectMultiScale(frame_escala_cinza, 1.3, 5)
+        k = 1
+        rostos = sorted(rostos, key=lambda x: x[2] * x[3], reverse=True)
+        skip += 1
+        for rosto in rostos:
+            x, y, w, h = rosto
 
-        # Only process every other frame of video to save time
-        if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            offset = 5
+            rosto_offset = frame_escala_cinza[y - offset:y + h + offset, x - offset:x + w + offset]
+            roi_gray = frame_escala_cinza[y - offset:y + h + offset]
+            id_,conf = recognizer.predict(roi_gray)
+            # print(conf) 
+            if conf >= 0 and conf <=100:
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                name = labels[id_]
+                color = (255,255,255)
+                stroke = 2
+                cv2.putText(frame,name,(x,y),font,1,color,stroke,cv2.LINE_AA)
+            try:
+                selecao_de_rosto = cv2.resize(rosto_offset, (100, 100))
+            except Exception as e:
+                # ignora imagens nas quais algum rosto esteja próximo a bora
+                print(str(e))
 
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
+            frameShape = frame.shape
+            mask = np.zeros((frameShape[0], frameShape[1], 3), dtype=np.uint8)
+            for i in range(y, y + h):
+                for j in range(x, x + w):
+                    mask[i][j][:] = 255
+            cv2.imshow('mask', mask)
+            blurredImage = cv2.GaussianBlur(frame, (35, 35), 0)
+            blurredFace = np.where(mask == np.array([255, 255, 255]), blurredImage, frame)
 
-                # # If a match was found in known_face_encodings, just use the first one.
-                # if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = known_face_names[first_match_index]
+            cv2.imshow(str(k), selecao_de_rosto)
+            k += 1
 
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
+            cv2.imshow("rostos_borrados", blurredFace)
 
-                face_names.append(name)
-
-        process_this_frame = not process_this_frame
-
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-        # Display the resulting image
-        cv2.imshow('Video', frame)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.imshow("rostos", frame)
+        key_pressed = cv2.waitKey(1) & 0xFF
+        if key_pressed == ord('q'):
+            break
